ab0faee2504a3719e74fec1f82219cba6c83e98d
Claire C <claire.chabas@gmail.com>
2022-10-29 17:51:52 +0200

    feat: displaying unarchived emails list

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,199 @@
 <template>
   <h1>VMail Inbox</h1>
+
+  <table class="mail-table">
+    <tbody>
+      <tr 
+        v-for="email in unarchivedEmails"
+        :key="email.id"
+        :class="['clickable', email.read ? 'read' : '']"
+        @click="email.read = true"
+      >
+        <td>
+          <input type="checkbox">
+        </td>
+        <td>{{ email.from }}</td>
+        <td>
+          <p><span class="subject">{{ email.subject }}</span> - {{ email.body }}</p>
+        </td>
+        <td class="date">{{ format(new Date(email.sentAt), 'MMM do yyyy') }}</td>
+        <td><button @click="email.archived = true">Archive</button></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
+<script>
+import { emails } from '../db.json'
+import { format } from 'date-fns'
+
+export default {
+  data() {
+    return {
+      format,
+      emails,
+    }
+  },
+  computed: {
+    sortedEmails() {
+      return this.emails.sort((email1, email2) => email1.sentAt < email2.sentAt ? 1 : -1)
+    },
+    unarchivedEmails() {
+      return this.sortedEmails.filter(email => !email.archived)
+    } 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/* Global Styles */
+button {
+  font-size: 16px;
+  padding: 8px;
+  border-radius: 3px;
+  margin: 5px 10px 5px 0px;
+  cursor: pointer;
+}
+
+button:disabled {
+  cursor: auto;
+}
+
+button.selected {
+  cursor: auto;
+  color: black;
+  border-color: black;
+  border-width: 2px;
+}
+
+.clickable {
+  cursor: pointer;
+}
+
+input[type='checkbox'] {
+  -webkit-appearance: none;
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  background: white;
+  border-radius: 2px;
+  border: 1px solid #555;
+  position: relative;
+  vertical-align: middle;
+  padding: 10px;
+}
+
+input[type='checkbox'].partial-check {
+  background: #ABC;
+}
+
+input[type='checkbox']:checked {
+  background: #679;
+}
+
+.mb-0 {
+  margin-bottom: 0;
+}
+
+/* Modal */
+.modal,
+.overlay {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+
+.overlay {
+  opacity: 0.5;
+  background-color: black;
+}
+
+.modal-card {
+  position: relative;
+  max-width: 80%;
+  margin: auto;
+  margin-top: 30px;
+  padding: 20px;
+  background-color: white;
+  min-height: 500px;
+  z-index: 10;
+  opacity: 1;
+}
+
+/* Email Modal */
+.email-display {
+  text-align: left;
+}
+
+/* Mail Table */
+.mail-table {
+  max-width: 1000px;
+  margin: auto;
+  border-collapse: collapse;
+}
+
+.mail-table tr.read {
+  background-color: #EEE;
+}
+
+.mail-table tr.read .subject {
+  font-weight: 400;
+}
+
+.mail-table tr td .subject {
+  font-weight: 600;
+}
+
+.mail-table tr {
+  height: 40px;
+}
+
+.mail-table td {
+  border-bottom: 1px solid black;
+  padding: 5px;
+  text-align: left;
+}
+
+.mail-table tr:first-of-type td {
+  border-top: 1px solid black;
+}
+
+.mail-table td p {
+  max-height: 1.2em;
+  overflow-y: hidden;
+  margin: 0;
+}
+
+.mail-table td.date {
+  width: 120px;
+}
+
+/* Bulk Action Bar */
+.bulk-action-bar {
+  width: 100%;
+  max-width: 1000px;
+  margin: auto;
+  text-align: left;
+  padding-bottom: 8px;
+}
+
+.bulk-action-bar input {
+  margin: 5px;
+}
+
+.bulk-action-bar .checkbox {
+  margin-right: 6px;
+  margin-left: 3px;
+}
+</style>
